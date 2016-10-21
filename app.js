7094d0a21f855f6a12114024de952e8a64750ccf
zacacollier <zacacollier@gmail.com>
2016-10-20 21:08:58 -0500

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// Create your app
+const express = require('express')
+const mongoose = require('mongoose');
+const TaskModel = require('./models/TaskModel');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb://localhost/task-list/');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  TaskModel.find((err, tasks) => {
+    res.json(tasks);
+  })
+})
+
+const port = 4000;
+
+app.listen(4000, () => console.log(`Listening on port ${port}`););
