0d54a76f144ae52f94f6beaf69e92cdf988f38a7
Canaan Davis <canaandavis@gmail.com>
2016-10-20 21:06:01 -0500

    Add body parser

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// Create your app
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.user(bodyParser.json());
+app.user(bodyParser.urlencoded({ extended: false }));
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
